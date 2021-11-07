c6898ede75949601d6503eb22bffba0b5bda8b90
Mariamopataraia <mariamopataraia@gmail.com>
2021-11-08 00:38:45 +0400

    update

--- app.js
+++ app.js
@@ -21,23 +21,75 @@ function currentTime() {
   
     document.getElementById("clock").innerText = time; 
     let t = setTimeout(function(){ currentTime() }, 1000);
-  }
-  
-  currentTime();
+}
+
+currentTime();
+
+
+// const getCurrentTimeDate = () => {
+//     let currentTimeDate = new Date();
+
+//     var weekday = new Array(7);
+//     weekday[0] = "SUN";
+//     weekday[1] = "MON";
+//     weekday[2] = "TUE";
+//     weekday[3] = "WED";
+//     weekday[4] = "THU";
+//     weekday[5] = "FRI";
+//     weekday[6] = "SAT";
+
+//     var month = new Array();
+//     month[0] = "JAN";
+//     month[1] = "FEB";
+//     month[2] = "MAR";
+//     month[3] = "APR";
+//     month[4] = "May";
+//     month[5] = "JUN";
+//     month[6] = "JUL";
+//     month[7] = "AUG";
+//     month[8] = "SEP";
+//     month[9] = "OCT";
+//     month[10] = "NOV";
+//     month[11] = "DEC";
+
+//     var hours   =  currentTimeDate.getHours();
+
+//     var minutes =  currentTimeDate.getMinutes();
+//     minutes = minutes < 10 ? '0'+minutes : minutes;
+
+//      var AMPM = hours >= 12 ? 'PM' : 'AM';
+
+//     if(hours === 12){
+//         hours=12;
+
+//     }else{
+
+//         hours = hours%12;
+
+//     }
+
+//     var currentTime = `${hours}:${minutes}${AMPM}`;
+//     var currentDay = weekday[currentTimeDate.getDay()];
+
+//     var currentDate  = currentTimeDate.getDate();
+//     var currentMonth = month[currentTimeDate.getMonth()];
+//     var CurrentYear = currentTimeDate.getFullYear();
+
+//     var fullDate = `${currentDate} ${currentMonth} ${CurrentYear}`;
+
+
+//     document.getElementById("time").innerHTML = currentTime;
+//     document.getElementById("day").innerHTML = currentDay;
+//     document.getElementById("date").innerHTML = fullDate;
+
+//     setTimeout(getCurrentTimeDate, 500);
+
+// }
+// getCurrentTimeDate();
+
+
 
 //   Task#2 - სლაიდერები
-
-
-// const countDown = document.querySelector('#count-down');
-//
-// const intervalId = setInterval(() => {
-//   countDown.textContent = (parseInt(countDown.textContent) - 1);
-// }, 1000);
-//
-// setTimeout(() => {
-//   // console.log("Stop interval");
-//   clearInterval(intervalId);
-// }, 4000);
 
 const slides = document.querySelectorAll('.slide-item');
 const slidesLength = slides.length;
@@ -63,7 +115,7 @@ function nextSlide() {
   }
 
   renderSlider();
-}
+}queueMicrotask,
 
 function prevSlide() {
   if(activeIndex === 0){
